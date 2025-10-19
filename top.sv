29d6abf03ed1c8b0882278340fb71bed6e1fc358
Shams M. Monem <shamsmmonem@gmail.com>
2025-10-19 19:01:56 +0300

    Removed CDC into DTM. simulation works but FPGA doesn't

--- top.sv
+++ top.sv
@@ -11,6 +11,7 @@ bit clk, sysrst_n,rst_n;
 assign rst_n = !ndmreset & sysrst_n;
 // Debugging
 bit tdo, tdo_en, tclk, tdi, tms, trst, tclk_en, tclk_gen;
+assign tdo = tdo_en & dtm_tdo;
 logic [33+7:0] drscan = 0;
 always tclk = tclk_en ? tclk_gen : 0;
 initial #2 forever #JTAG_PERIOD tclk_gen = ~tclk_gen;
@@ -57,65 +58,17 @@ dbus_interconnect dbus_ic(.*);
 ibus_interconnect ibus_ic(.*);
 
 // Debug
-// CDC synchronizes needed between JTAG and System clock
 
-bit dmi_start_dtm;
-logic [1:0] dmi_op_dtm;
-logic [33:2] dmi_data_o_dtm, dmi_data_i_dtm;
-logic [7+33:34] dmi_address_dtm;
-logic dmi_finish_dtm;
-dtm_jtag debug_transport(.tdi(tdi), .trst(trst), .tms(tms), .tclk(tclk), .tdo(tdo), .tdo_en(tdo_en), .dmi_start(dmi_start_dtm), .dmi_op(dmi_op_dtm), .dmi_data_o(dmi_data_o_dtm), .dmi_address(dmi_address_dtm), .dmi_finish(dmi_finish_dtm), .dmi_data_i(dmi_data_i_dtm));
+bit dmi_start;
+logic [1:0] dmi_op;
+logic [33:2] dmi_data_o, dmi_data_i;
+logic [7+33:34] dmi_address;
+logic dmi_finish;
+logic dtm_tdo;
+dtm_jtag debug_transport(.tdi(tdi), .trst(trst), .tms(tms), .tclk(tclk), .tdo(dtm_tdo), .tdo_en(tdo_en), .dmi_start(dmi_start), .dmi_op(dmi_op), .dmi_data_o(dmi_data_o), .dmi_address(dmi_address), .dmi_finish(dmi_finish), .dmi_data_i(dmi_data_i), .clk(clk), .rst_n(rst_n));
 
 logic ndmreset;
-bit dmi_start_dm;
-logic [1:0] dmi_op_dm;
-logic [33:2] dmi_data_o_dm, dmi_data_i_dm;
-logic [7+33:34] dmi_address_dm;
-logic dmi_finish_dm;
-dm debug_module(.haltreq(haltreq), .resumereq(resumereq), .resethaltreq(resethaltreq), .halted(halted), .running(running), .clk(clk), .rst_n(rst_n), .ndmreset(ndmreset), .dmi_start(dmi_start_dm), .dmi_op(dmi_op_dm), .dmi_data_o(dmi_data_o_dm), .dmi_address(dmi_address_dm), .dmi_finish(dmi_finish_dm), .dmi_data_i(dmi_data_i_dm));
-
-// CDC
-
-typedef struct packed {bit start; logic [1:0] op; logic [33:2] data; logic [7+33:34] address;} dmi_to_dm_t;
-dmi_to_dm_t dmi_to_dm, dmi_to_dm_ff1, dmi_to_dm_ff2;
-
-typedef struct packed {bit finish; logic [33:2] data;} dm_to_dmi_t;
-dm_to_dmi_t dm_to_dmi, dm_to_dmi_ff1, dm_to_dmi_ff2;
-always_comb begin
-    // ff0
-    dmi_to_dm = {dmi_start_dtm, dmi_op_dtm, dmi_data_o_dtm, dmi_address_dtm};
-    dm_to_dmi = {dmi_finish_dm, dmi_data_i_dm};
-
-    // connect
-    dmi_start_dm = dmi_to_dm_ff2.start;
-    dmi_op_dm = dmi_to_dm_ff2.op;
-    dmi_data_o_dm = dmi_to_dm_ff2.data;
-    dmi_address_dm = dmi_to_dm_ff2.address;
-
-    dmi_finish_dtm = dm_to_dmi_ff2.finish;
-    dmi_data_i_dtm = dm_to_dmi_ff2.data;
-end
-
-always_ff @(posedge clk, negedge rst_n) begin
-    if (!rst_n) begin
-        dmi_to_dm_ff1 <= 0;
-        dmi_to_dm_ff2 <= 0;
-    end else begin
-        dmi_to_dm_ff1 <= dmi_to_dm;
-        dmi_to_dm_ff2 <= dmi_to_dm_ff1;
-    end
-end
-
-always_ff @(posedge tclk, negedge trst) begin
-    if (!trst) begin
-        dm_to_dmi_ff1 <= 0;
-        dm_to_dmi_ff2 <= 0;
-    end else begin
-        dm_to_dmi_ff1 <= dm_to_dmi;
-        dm_to_dmi_ff2 <= dm_to_dmi_ff1;
-    end
-end
-
+dm debug_module(.haltreq(haltreq), .resumereq(resumereq), .resethaltreq(resethaltreq), .halted(halted), .running(running), .clk(clk), .rst_n(rst_n), .ndmreset(ndmreset), .dmi_start(dmi_start), .dmi_op(dmi_op), .dmi_data_o(dmi_data_o), .dmi_address(dmi_address), .dmi_finish(dmi_finish), .dmi_data_i(dmi_data_i));
 
 // assertions and coverage
 property dbus_access_valid;
@@ -189,7 +142,7 @@ initial begin
     @(posedge tclk);
     #1;
     update_ir(6'h11); // access DMI
-    read_dr41({7'h11, 32'h00000000, 2'b01}, drscan);
+    read_dr41(41'h10800000002, drscan);
     #1000
 
 
